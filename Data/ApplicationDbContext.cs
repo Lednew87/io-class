0698bb33846409ef9df06f151d4012c1a3e5a923
Wendel Tonzar <wendeltonzar@gmail.com>
2023-10-21 14:00:43 -0300

    Estrutura do projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TesteMVC.Models;
+
+namespace TesteMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TesteMVC.Models.Aluno> Aluno { get; set; } = default!;
+    }
+}
